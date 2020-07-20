0259e6a5befc4c6ca34f83c4a8b744bcc266d95d
Rahit Saha <65773180+Infra17@users.noreply.github.com>
2020-07-20 13:18:24 -0700

    Create Dole Out Cadbury(Codevita).cpp

---
+++ Dole Out Cadbury(Codevita).cpp
@@ -0,0 +1,75 @@
+//Problem statement given below
+//Author : Infra
+//Status : Right ANswer
+//Author : Infra
+//Platform : Codevita
+#include<bits/stdc++.h>
+using namespace std;
+int dp[1502][1502];
+/*
+int func(int mx,int mn)
+{
+if(dp[mx][mn]!=0)
+return dp[mx][mn];
+if(mx==mn)
+{dp[mx][mn]=1;return 1;}
+dp[mx][mn] = 1+func(max(mn,mx-mn),min(mn,mx-mn));
+return dp[mx][mn];
+}
+*/
+int main()
+{
+int minl,maxl,minw,maxw,sum=0;
+cin>>minl>>maxl>>minw>>maxw;
+/*for(int i=minl;i<=maxl;i++)
+for(int j=minw;j<=maxw;j++)
+sum+=func(max(i,j),min(i,j));
+*/
+for(int i=1;i<=maxl;i++)
+for(int j=1;j<=maxw;j++)
+{
+if(i==j)
+dp[i][j]=1;
+else if(i==1||j==1)
+dp[i][j]=dp[j][i]=max(i,j);
+else
+dp[i][j]=dp[j][i]=1+dp[min(i,j)][max(i,j)-min(i,j)];
+}
+for(int i=minl;i<=maxl;i++)
+for(int j=minw;j<=maxw;j++)
+sum+=dp[i][j];
+cout<<sum;
+return 0;
+}
+
+/*
+Dole Out Cadbury
+Problem Description
+You are a teacher in reputed school. During Celebration Day you were assigned a task to distribute Cadbury such that maximum children get the chocolate. You have a box full of Cadbury with different width and height. You can only distribute largest square shape Cadbury. So if you have a Cadbury of length 10 and width 5, then you need to break Cadbury in 5X5 square and distribute to first child and then remaining 5X5 to next in queue
+Constraints
+0<P<Q<1501
+0<R<S<1501
+Input Format
+First line contains an integer P that denotes minimum length of Cadbury in the box
+Second line contains an integer Q that denotes maximum length of Cadbury in the box
+Third line contains an integer R that denotes minimum width of Cadbury in the box
+Fourth line contains an integer S that denotes maximum width of Cadbury in the box
+Output
+Print total number of children who will get chocolate.
+Timeout
+1
+Explanation
+Example 1
+Input
+5
+7
+3
+4
+Output
+24
+Explanation
+Length is in between 5 to 7 and width is in between 3 to 4.
+So we have 5X3,5X4,6X3,6X4,7X3,7X4 type of Cadbury in the box.
+If we take 5X3 :
+First, we can give 3X3 square Cadbury to 1st child .Then we are left with 3X2. Now largest square is 2X2 which will be given to next child. Next, we are left with two 1X1 part of Cadbury which will be given to another two children.
+And so on*/
