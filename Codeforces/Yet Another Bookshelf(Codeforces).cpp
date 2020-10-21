3681763727e7aa0cd1d151721f2a5c61b5ae54e3
Rahit Saha <65773180+Infra17@users.noreply.github.com>
2020-10-21 15:03:04 +0530

    Create Yet Another Bookshelf(Codeforces).cpp

---
+++ Codeforces/Yet Another Bookshelf(Codeforces).cpp
@@ -0,0 +1,94 @@
+//problem Given Below
+//Author : Infra
+//Status : Accepted
+
+#include<bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define booster cout.tie(NULL)
+typedef long long int lld;
+#define mod 1000000007
+using namespace std;
+
+int main()
+{
+    int t,s,ss,n;cin>>t;
+    while(t--)
+    {
+        cin>>n;int flag=0,L,R;ss=0;int ans=0;
+        int *a= new int[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>s;a[i]=s;
+            ss+=s;
+            if(flag==1&&s==1)
+            R=i;
+            else if(flag==0&&s==1)
+            {L=i;R=i;flag=1;}
+        }
+        int p=R-L+1;
+        while(p>ss)
+            {int q=R;
+                while(a[q]!=0) q--;
+                p--;R--;a[q]=1;ans++;
+            }
+cout<<ans<<endl;
+    }
+}
+
+/*
+Contest Round #677, Div 3
+B. Yet Another Bookshelf
+time limit per test1 second
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+There is a bookshelf which can fit n books. The i-th position of bookshelf is ai=1 if there is a book on this position and ai=0 otherwise. It is guaranteed that there is at least one book on the bookshelf.
+
+In one move, you can choose some contiguous segment [l;r] consisting of books (i.e. for each i from l to r the condition ai=1 holds) and:
+
+Shift it to the right by 1: move the book at index i to i+1 for all l≤i≤r. This move can be done only if r+1≤n and there is no book at the position r+1.
+Shift it to the left by 1: move the book at index i to i−1 for all l≤i≤r. This move can be done only if l−1≥1 and there is no book at the position l−1.
+Your task is to find the minimum number of moves required to collect all the books on the shelf as a contiguous (consecutive) segment (i.e. the segment without any gaps).
+
+For example, for a=[0,0,1,0,1] there is a gap between books (a4=0 when a3=1 and a5=1), for a=[1,1,0] there are no gaps between books and for a=[0,0,0] there are also no gaps between books.
+
+You have to answer t independent test cases.
+
+Input
+The first line of the input contains one integer t (1≤t≤200) — the number of test cases. Then t test cases follow.
+
+The first line of the test case contains one integer n (1≤n≤50) — the number of places on a bookshelf. The second line of the test case contains n integers a1,a2,…,an (0≤ai≤1), where ai is 1 if there is a book at this position and 0 otherwise. It is guaranteed that there is at least one book on the bookshelf.
+
+Output
+For each test case, print one integer: the minimum number of moves required to collect all the books on the shelf as a contiguous (consecutive) segment (i.e. the segment without gaps).
+
+Example
+inputCopy
+5
+7
+0 0 1 0 1 0 1
+3
+1 0 0
+5
+1 1 0 0 1
+6
+1 0 0 0 0 1
+5
+1 1 0 1 1
+outputCopy
+2
+0
+2
+4
+1
+Note
+In the first test case of the example, you can shift the segment [3;3] to the right and the segment [4;5] to the right. After all moves, the books form the contiguous segment [5;7]. So the answer is 2.
+
+In the second test case of the example, you have nothing to do, all the books on the bookshelf form the contiguous segment already.
+
+In the third test case of the example, you can shift the segment [5;5] to the left and then the segment [4;4] to the left again. After all moves, the books form the contiguous segment [1;3]. So the answer is 2.
+
+In the fourth test case of the example, you can shift the segment [1;1] to the right, the segment [2;2] to the right, the segment [6;6] to the left and then the segment [5;5] to the left. After all moves, the books form the contiguous segment [3;4]. So the answer is 4.
+
+In the fifth test case of the example, you can shift the segment [1;2] to the right. After all moves, the books form the contiguous segment [2;5]. So the answer is 1.
+*/
