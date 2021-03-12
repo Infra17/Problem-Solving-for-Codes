fb4a2c45f83718d2abac9cb3aed29bf20b89793c
Rahit Saha <65773180+Infra17@users.noreply.github.com>
2021-03-12 18:38:45 +0530

    Update Number of Likes and Dislikes.cpp

--- Easy Implement for Beginners Practice/Number of Likes and Dislikes.cpp
+++ Easy Implement for Beginners Practice/Number of Likes and Dislikes.cpp
@@ -1,39 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-int b;
-
-int answer(string a,int i,int ts,int fs,int count)
-{
-    if(i<0)
-    return INT_MAX;
-    if(i==0)
-      if(ts==b) {return count;}
-       else return INT_MAX;
-
-    if(ts<b) return INT_MAX;
-
-    string s1=a.substr(0,i),s2=a.substr(i,a.length()-i);
-    int x=stoi(s1)+stoi(s2)+fs;
-    int y=fs+stoi(s2);
-    return min(answer(s1,i-1,x,y,count+1),answer(a,i-1,ts,fs,count));
-
-}
 
 int main()
 {
-    string s;
-    cin>>s;
-    int i;
-    for(i=0;i<s.length();i++)
-    if(s[i]=='=') break;
-
-    string A=s.substr(0,i);
-    string B=s.substr(i+1,s.length()-i-1);
-    int a=stoi(A);
-    b=stoi(B);
-    int ans=answer(A,A.length()-1,a,0,0);
-    if(ans==INT_MAX) cout<<-1;
-    else cout<<ans;
+    string a,b;
+    cin>>a>>b;
+    int l=a.length();
+    int aa = stoull(a, 0, 2),bb=stoull(b, 0, 2);
+    int x=(aa^bb);
+    int ans=0;
+    while(x)
+    {
+        x&=(x-1);
+        ans++;
+    }
+    cout<<l-ans;
+    return 0;
 }
 
 /*
