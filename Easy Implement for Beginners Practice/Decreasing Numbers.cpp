200eed774280003ad147b2daa53e103113ede954
Rahit Saha <65773180+Infra17@users.noreply.github.com>
2021-04-19 13:34:53 +0530

    Create Decreasing Numbers.cpp

---
+++ Easy Implement for Beginners Practice/Decreasing Numbers.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int ans;
+void func(int a,int l,string s,int z,int p)
+{
+    //cout<<s<<endl;
+    if(z==l) {if(a>=stoi(s)) 
+    {//cout<<s<<endl;
+    ans++;} return; }
+    for(int i=p;i>=0;i--)
+    func(a,l,s+to_string(i),z+1,i);
+
+}
+
+
+int main()
+{
+    string s;
+    cin>>s;ans=0;
+    int l=s.length();
+    func(stoi(s),l,"",0,9);
+    cout<<ans;
+}
+/*
+Decreasing Numbers
+A non-increasing subsequence is a sequence in which each subsequent element is not greater than the previous element.
+You are given a number N. Calculate the number of numbers <= N whose decimal representation forms a non-increasing subsequence.
+Example – 531 has a non-increasing decimal representation while 591 does not have a non-increasing decimal representation because 5<9.
+*/
