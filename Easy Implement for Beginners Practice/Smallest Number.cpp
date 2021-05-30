6804b8caf142d235e7ef2da3d883e42532840639
Rahit Saha <65773180+Infra17@users.noreply.github.com>
2021-05-30 18:54:25 +0530

    Update Smallest Number.cpp

--- Easy Implement for Beginners Practice/Smallest Number.cpp
+++ Easy Implement for Beginners Practice/Smallest Number.cpp
@@ -1,38 +1,24 @@
-#include <iostream>
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int smallestNumber (string num, int k)
-{
-    if(num.length()<=k)
-        return 0;
-    unordered_map<char,int> pos;
-    for(int i=0;i<num.length();i++)
-   { pos[num[i]]=i;}
 
-    string temp=num;
-    sort(num.begin(),num.end());
-    string ans=num.substr(0,num.length()-k);
-    vector<int> v;
-    for(int i=0;i<ans.length();i++)
-    v.push_back(pos[ans[i]]);
-
-    sort(v.begin(),v.end());
-    string ret;
-    for(int i=0;i<v.size();i++)
-    {ret+=temp[v[i]];}
-    int final=stoi(ret);
-
-    return final;
-}
 int main()
 {
-    string s;
-    cin >> s;
-    int k;
-    cin >> k;
-    int ans;
-    cout<<smallestNumber(s,k)%(int)(pow(10,9)+7);
-    return 0;
+    string s;int k;
+    cin>>s>>k;
+    string y=s;
+    sort(y.begin(),y.end());cout<<y<<endl;
+    y=y.substr(y.length()-k,k);cout<<y<<endl;
+    map<char,int> m;
+    for(auto i:y)
+    m[i]++;
+
+    string ans="";
+    for(auto i:s)
+    if(m[i])
+    {m[i]--;}
+    else ans+=i;
+
+    cout<<ans;
 }
 
 /*
